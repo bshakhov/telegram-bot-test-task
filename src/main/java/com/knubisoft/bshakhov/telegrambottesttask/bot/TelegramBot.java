6841b97f07ebe59e7503e907a52da49c8c0837a1
Shakhov Bohdan <b.shakhov@knubisoft.com>
2023-10-10 14:05:21 +0300

    Implement notification algorithm

--- src/main/java/com/knubisoft/bshakhov/telegrambottesttask/bot/TelegramBot.java
+++ src/main/java/com/knubisoft/bshakhov/telegrambottesttask/bot/TelegramBot.java
@@ -1,7 +1,8 @@
 package com.knubisoft.bshakhov.telegrambottesttask.bot;
 
 import com.knubisoft.bshakhov.telegrambottesttask.config.TelegramBotConfig;
-import com.knubisoft.bshakhov.telegrambottesttask.service.CryptoPriceService;
+import com.knubisoft.bshakhov.telegrambottesttask.dao.entity.User;
+import com.knubisoft.bshakhov.telegrambottesttask.dao.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -13,6 +14,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 @AllArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
     private final TelegramBotConfig botConfig;
+    private final UserRepository userRepository;
 
     @Override
     public String getBotUsername() {
@@ -26,40 +28,85 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        String cryptoPrice;
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
             switch (messageText) {
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                default:
-                    cryptoPrice = CryptoPriceService.getCurrencyRate();
-                    sendMessage(chatId, cryptoPrice);
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "/commands" -> sendListOfAvailableCommands(chatId);
+                case "/changePercent" -> sendMessage(chatId, "Please provide new percent", "/changePercent");
+                case "/changeRefreshFrequency" ->
+                        sendMessage(chatId, "Please provide new refresh frequency in seconds", "/changeRefreshFrequency");
+                default -> {
+                    User user = userRepository.findByChatId(chatId);
+                    if (user.getLastCommand().equals("/changePercent")) {
+                        String changePercentMessage = update.getMessage().getText();
+                        try {
+                            double updatedPercent = Double.parseDouble(changePercentMessage);
+                            user.setChangingPercent(updatedPercent);
+                            userRepository.save(user);
+                            sendMessage(chatId, "Percent successfully updated");
+                        } catch (NumberFormatException e) {
+                            sendMessage(chatId, "You have provided incorrect value");
+                        }
+                    } else if (user.getLastCommand().equals("/changeRefreshFrequency")) {
+                        String changeRefreshFrequency = update.getMessage().getText();
+                        try {
+                            int updatedRefreshFrequency = Integer.parseInt(changeRefreshFrequency);
+                            user.setUpdateFrequency(updatedRefreshFrequency);
+                            userRepository.save(user);
+                            sendMessage(chatId, "Refresh frequency successfully updated");
+                        } catch (NumberFormatException e) {
+                            sendMessage(chatId, "You have provided incorrect value");
+                        }
+                    }
+                }
             }
         }
 
     }
 
-    private void startCommandReceived(Long chatId, String name) {
-        String answer = "Hi, " + name + ", nice to meet you!" + "\n" +
-                        "Enter the currency whose official exchange rate" + "\n" +
-                        "you want to know in relation to BYN." + "\n" +
-                        "For example: USD";
+    private void startCommandReceived(final Long chatId, final String name) {
+        userRepository.save(User.builder()
+                .chatId(chatId)
+                .lastCommand("/start")
+                .changingPercent(2.5)
+                .updateFrequency(60)
+                .build());
+        String answer = "Hi, " + name + ", nice to meet you!";
         sendMessage(chatId, answer);
     }
 
-    private void sendMessage(Long chatId, String textToSend) {
+    private void sendListOfAvailableCommands(final Long chatId) {
+        User user = userRepository.findByChatId(chatId);
+        user.setLastCommand("/commands");
+        userRepository.save(user);
+        String answer = """
+                /commands - show list of all available commands
+                /changePercent - if some cryptocurrency becomes more expensive or cheaper by more than N percent yo will be notified
+                /changeRefreshFrequency - Algorithm should make a request and refresh application state every S seconds
+                """;
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(final Long chatId, final String textToSend, final String commandName) {
+        User user = userRepository.findByChatId(chatId);
+        user.setLastCommand(commandName);
+        userRepository.save(user);
+        sendMessage(chatId, textToSend);
+    }
+
+    public void sendMessage(final Long chatId, final String textToSend) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
         sendMessage.setText(textToSend);
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
-
+            System.err.println(e.getMessage());
         }
     }
+
 
 }
--- src/main/java/com/knubisoft/bshakhov/telegrambottesttask/service/CryptoPriceService.java
+++ src/main/java/com/knubisoft/bshakhov/telegrambottesttask/service/CryptoPriceService.java
@@ -1,30 +1,70 @@
 package com.knubisoft.bshakhov.telegrambottesttask.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.knubisoft.bshakhov.telegrambottesttask.bot.TelegramBot;
+import com.knubisoft.bshakhov.telegrambottesttask.dao.entity.Crypto;
+import com.knubisoft.bshakhov.telegrambottesttask.dao.entity.User;
+import com.knubisoft.bshakhov.telegrambottesttask.dao.repository.CryptoRepository;
+import com.knubisoft.bshakhov.telegrambottesttask.dao.repository.UserRepository;
 import com.knubisoft.bshakhov.telegrambottesttask.model.CryptoPriceResponse;
+import lombok.RequiredArgsConstructor;
 import okhttp3.*;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+@RequiredArgsConstructor
+@Service
 public class CryptoPriceService {
 
     private static final String CRYPTO_PRICE_SOURCE_URL = "https://api.mexc.com/api/v3/ticker/price";
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final int FIXED_DELAY_DURATION = 30000;
 
-    public static String getCurrencyRate() {
+    private final TelegramBot telegramBot;
+    private final CryptoRepository cryptoRepository;
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedDelay = FIXED_DELAY_DURATION)
+    public void getCurrencyRate() {
         String responseBody = makeRequestAndGetResponseAsString(CRYPTO_PRICE_SOURCE_URL);
-        CryptoPriceResponse[] cryptoPriceResponse = mapResponseToJavaObj(responseBody);
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < 5; i++) {
-            stringBuilder.append(cryptoPriceResponse[i].getSymbol()).append(": ")
-                    .append(cryptoPriceResponse[i].getPrice()).append("\n");
-
+        CryptoPriceResponse[] cryptoPriceResponseList = mapResponseToJavaObj(responseBody);
+        List<Crypto> newCryptoList = new LinkedList<>();
+        for (CryptoPriceResponse cryptoPriceResponse : cryptoPriceResponseList) {
+            newCryptoList.add(Crypto.builder()
+                    .symbol(cryptoPriceResponse.getSymbol())
+                    .price(cryptoPriceResponse.getPrice())
+                    .build());
+        }
+        List<Crypto> previousCryptoList = cryptoRepository.findAll();
+        if (previousCryptoList.isEmpty()) {
+            cryptoRepository.saveAll(newCryptoList);
+            return;
         }
 
-
-        return stringBuilder.toString();
+        for (int i = 0; i < newCryptoList.size(); i++) {
+            Crypto newCrypto = newCryptoList.get(i);
+            Crypto previousCrypto = previousCryptoList.get(i);
+            double changingPercent = calculatePriceChangingInPercent(Double.parseDouble(previousCrypto.getPrice()),
+                    Double.parseDouble(newCrypto.getPrice()));
+            if (changingPercent < 0) {
+                changingPercent = changingPercent * -1;
+            }
+            List<User> allUsers = userRepository.findAll();
+            for (User user : allUsers) {
+                if (changingPercent > user.getChangingPercent()) {
+                    String textToSend = "The price of " + previousCrypto.getSymbol() + " cryptocurrency has changed.\n" +
+                                           "by " + changingPercent + "%\n" +
+                                           "Previous price: " + previousCrypto.getPrice() +
+                                           "\nNew Price: " + newCrypto.getPrice();
+                    telegramBot.sendMessage(user.getChatId(), textToSend);
+                }
+            }
+        }
 
     }
 
@@ -48,6 +88,16 @@ public class CryptoPriceService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static double calculatePriceChangingInPercent(double prevValue, double newValue) {
+        double percent;
+        if (newValue > prevValue) {
+            percent = (((newValue - prevValue) * 100) / prevValue);
+        } else {
+            percent = (((prevValue - newValue) * 100) / prevValue) * -1;
+        }
+        return percent;
     }
 
 }
