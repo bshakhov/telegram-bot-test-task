011f0b8d03b568b0c598222ea90e8eeb0d0159ef
Shakhov Bohdan <b.shakhov@knubisoft.com>
2023-10-10 10:33:49 +0300

    Initial Commit

---
+++ src/main/java/com/knubisoft/bshakhov/telegrambottesttask/TelegramBotTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.knubisoft.bshakhov.telegrambottesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knubisoft/bshakhov/telegrambottesttask/TelegramBotTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knubisoft.bshakhov.telegrambottesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
